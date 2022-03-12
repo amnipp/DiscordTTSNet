24ccb96621d856aa5cc2e515dcf1637441db2b2b
Andrew Nipp <andrewnipp@yahoo.com>
2022-03-12 13:54:50 -0800

    Added skeletal structure

--- DiscordTTS.cs
+++ DiscordTTS.cs
@@ -25,14 +25,8 @@ namespace DiscordTTS
             string jsonString = r.ReadToEnd();
             // TODO: store these into settings 
             var config = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString);
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            var token = config["token"];
 
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            var token = config["token"];
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
---
+++ Helpers/InputParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordTTS.Helpers
+{
+    public static class InputParser
+    {
+        public static List<string> ParseInput(string input)
+        {
+            return new List<string>();
+        }
+    }
+}
